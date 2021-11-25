e9b663395ab35a60786e884938d8b2eedc312bf3
Mehmet Ardıç <ardicmehmet96@gmail.com>
2021-11-25 13:47:11 +0300

    sortAlgorithms
    sort algoritması ile veri sıralayan bir kod örneği

---
+++ sortAlgorithms.js
@@ -0,0 +1,73 @@
+let deneme = [{
+    "id": 1,
+    "details": [
+        {
+            "productId": 17,
+            "unitPrice": 10,
+            "quantity": 10,
+            "discount": 0
+        },
+        {
+            "productId": 46,
+            "unitPrice": 1,
+            "quantity": 5,
+            "discount": 0
+        }
+    ]
+},
+{
+    "id": 2,
+    "details": [
+        {
+            "productId": 17,
+            "unitPrice": 1,
+            "quantity": 2,
+            "discount": 0
+        },
+        {
+            "productId": 46,
+            "unitPrice": 1,
+            "quantity": 2,
+            "discount": 0
+        }
+    ]
+},
+{
+    "id": 3,
+    "details": [
+        {
+            "productId": 17,
+            "unitPrice": 1,
+            "quantity": 2,
+            "discount": 0
+        },
+        {
+            "productId": 46,
+            "unitPrice": 4,
+            "quantity": 2,
+            "discount": 0
+        }
+    ]
+},
+
+]
+let toplamA = 0
+let toplamB = 0
+deneme.sort((a, b) => {
+    toplamA = 0
+    toplamB = 0
+    a.details.forEach(x => {
+        toplamA = toplamA + x.unitPrice * x.quantity
+    })
+    b.details.forEach(x => {
+        toplamB = toplamB + x.unitPrice * x.quantity
+    })
+    if (toplamA > toplamB) {
+        return 1
+    }
+    else {
+        return -1
+    }
+
+})
+console.log(deneme)
